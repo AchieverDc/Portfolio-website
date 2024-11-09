9f6856d7393eb3e4bd2137073c1d0558a23bbd31
AchieverDc <achiva3@proton.me>
2024-11-09 10:55:51 -0800

    Portfolio Website (2nd Commit)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    if (sidebar.style.width === '350px') {
+    if (sidebar.style.width === '300px') {
         sidebar.style.width = '0';
     } else {
-        sidebar.style.width = '350px';
+        sidebar.style.width = '300px';
     }
 }
 
