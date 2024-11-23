772d782616e1c4c5f81a015f092651470cde6471
AchieverDc <achiva3@proton.me>
2024-11-22 23:59:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.post('/', (req, res)=>{
     console.log(req.body);
 
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
