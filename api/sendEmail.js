d0eed1648a2fcea54ae636c9adb7516f8ccbef61
AchieverDc <160872912+AchieverDc@users.noreply.github.com>
2025-07-26 22:50:41 -0700

    Updated NodeMailer

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 // This part is not needed for serving static files
 // const express = require('express');
@@ -12,35 +12,35 @@ const PORT = process.env.PORT || 5000;
 
 // API route for sending emails
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { email, phone, interest, message } = req.body;
+  if (req.method === "POST") {
+    const { email, subject, message } = req.body;
 
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-        const mailOptions = {
-            from: email,
-            to: process.env.EMAIL_USER,
-            subject: `From: ${email}\nPhone: ${phone}\nInterest: ${interest}`,
-            text: message,
-        };
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `From: ${email}\nSubject: ${subject}`,
+      text: message,
+    };
 
-        try {
-            await transporter.sendMail(mailOptions);
-            res.status(200).json({ message: 'Email sent successfully!' });
-        } catch (error) {
-            console.error('Error sending email:', error);
-            res.status(500).json({ error: 'Failed to send email.' });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Email sent successfully!" });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ error: "Failed to send email." });
     }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
